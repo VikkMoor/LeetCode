e161df648d940aa6939707fb691b072439018561
VikkMoor <moorvikk@gmail.com>
2024-05-28 18:24:38 +0500

    prefix_was_finally_understood

--- string/easy/CommonPrefix.java
+++ string/easy/CommonPrefix.java
@@ -6,8 +6,8 @@ public class CommonPrefix {
 
     public static void main(String[] args) {
         CommonPrefix letters = new CommonPrefix();
-        String[] test = {"flower","flow","flight"};
-        //System.out.println(Arrays.toString(test)); // (showing array in the right way just for cheking);
+        String[] test = {"flower","flow","flight", "fly"};
+        System.out.println(Arrays.toString(test)); // (showing array in the right way just for checking);
         System.out.println('"' + letters.longestCommonPrefix(test) + '"');
 
     }
@@ -17,18 +17,20 @@ public class CommonPrefix {
         if (size == 0) return ""; // if no words - so no prefix;
         if (size == 1) return strs[0]; // if one word - so the prefix is the whole word;
 
-        /* whaaaaat kind of sort is here??? */
+        /* sorts strings alphabetically!!!
+        * so than we can compare only first and last strings to find some common letters */
         Arrays.sort(strs);
+        System.out.println(Arrays.toString(strs)); // just for checking;
 
-        /* dunno what's going on here too */
+        /* finding min length between first and last elements in strs[]; */
         int end = Math.min(strs[0].length(), strs[size - 1].length());
-
+        System.out.println(end); // just for checking;
         /* find the common prefix between the first and last string */
         int i = 0;
         while (i < end && strs[0].charAt(i) == strs[size - 1].charAt(i))
             i++;
 
-        return strs[0].substring(0, i);
+        return strs[0].substring(0, i); // substring shows the part of string from 0 to (i-1);
     }
 
 }
