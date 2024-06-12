cd818355e23c683b989e9c36018d1b5b41a47b97
VikkMoor <moorvikk@gmail.com>
2024-06-12 10:30:55 +0500

    rightBrackets!!!finally

--- string/easy/ValidParentheses.java
+++ string/easy/ValidParentheses.java
@@ -1,35 +1,43 @@
 package easy;
 
+import java.util.*;
+
 public class ValidParentheses {
     public static void main(String[] args) {
         ValidParentheses res = new ValidParentheses();
-        String test = "(){}[]()";
+        String test = "()";
         System.out.println(res.isValid(test));
     }
 
-    public char bracket(char r) {
-        if (r == '(') return ')';
-        if (r == '{') return '}';
-        if (r == '[') return ']';
-        return '1';
-    }
 
     public boolean isValid(String s) {
         boolean a = true;
-        for (int i = 0; i < s.length(); i++) {
-            char s1 = s.charAt(i);
-            if (i + 1 < s.length()) {
-                char s2 = s.charAt(i + 1);
-                if (s2 != bracket(s1)) {
-                    a = false;
-                    break;
+
+        if (s.charAt(s.length() - 1) == '(' || s.charAt(s.length() - 1) == '{' || s.charAt(s.length() - 1) == '['){
+            a = false; // for the case as ((;
+        }
+        if (s.length() % 2 != 0) {
+            a = false;
+        } else {
+            // create the map(key, value) to set brackets compliance:
+            Map<Character, Character> both = new HashMap<>();
+            both.put(')', '(');
+            both.put('}', '{');
+            both.put(']', '[');
+            // create deque to use it like a stack:
+            Deque<Character> stack = new LinkedList<>();
+            for (char i : s.toCharArray()) { // for each i(char) in string s;
+                if (both.containsValue(i)) { // if some value in map "both" includes "i";
+                    stack.push(i); // if opening bracket, so push it to the end of deque;
+                } else if (both.containsKey(i)) { // if some key in map "both" includes "i";
+                    if (stack.isEmpty() || stack.pop() != both.get(i)) {
+                        a = false; /* both.get(i) - returns value for key "i" from "both" map;
+                                  stack.pop() - returns and removes the last char in queue "stack";*/
+                        break;
+                    }
                 }
-                else{
-                    i++;
-                }
-            } else {
-                a = false; // without this 'else' if one more alone bracket in the end, result will be 'true'
             }
+            if (stack.isEmpty() != true) a = false; // for the case as [[[];
         }
         return a;
     }
