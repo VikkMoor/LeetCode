e6ccd932410a7601263569fe72349c965ed7c63e
VikkMoor <moorvikk@gmail.com>
2024-06-14 22:27:49 +0500

    task4_begin

---
+++ string/easy/FirstOccurrence.java
@@ -0,0 +1,16 @@
+package easy;
+
+public class FirstOccurrence {
+    public static void main(String[] args) {
+        FirstOccurrence occ = new FirstOccurrence();
+        String test1 = "leetcode";
+        String test2 = "leetc";
+
+        System.out.println("Meow");
+    }
+
+    public int strStr(String haystack, String needle) {
+
+
+    }
+}
