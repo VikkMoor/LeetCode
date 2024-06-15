231dc50cbeece1bec6b6eb6150606f287716fab9
VikkMoor <moorvikk@gmail.com>
2024-06-15 10:44:29 +0500

    twosumstarted

---
+++ array/easy/TwoSum.java
@@ -0,0 +1,15 @@
+package easy;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        System.out.println("Meow");
+    }
+
+
+    //ublic int[] twoSum(int[] nums, int target) {
+
+
+
+    //return ?;
+    }
+}
