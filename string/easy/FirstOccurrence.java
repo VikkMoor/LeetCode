b8d2f1753d763c293e21a73e1f9aed6dacfd6ab0
VikkMoor <moorvikk@gmail.com>
2024-06-15 10:39:45 +0500

    firstOccurrenceReady;D

--- string/easy/FirstOccurrence.java
+++ string/easy/FirstOccurrence.java
@@ -3,14 +3,12 @@ package easy;
 public class FirstOccurrence {
     public static void main(String[] args) {
         FirstOccurrence occ = new FirstOccurrence();
-        String test1 = "leetcode";
-        String test2 = "leetc";
-
-        System.out.println("Meow");
+        String test1 = "upleetcodelee";
+        String test2 = "lee";
+        System.out.println(occ.strStr(test1, test2));
     }
 
     public int strStr(String haystack, String needle) {
-
-
+        return haystack.indexOf(needle);
     }
 }
