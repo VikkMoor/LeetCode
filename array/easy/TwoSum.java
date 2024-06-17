4048cf914e697d26d9b2ace51deb83ec11141036
VikkMoor <moorvikk@gmail.com>
2024-06-17 21:23:23 +0500

    twosumfinished

--- array/easy/TwoSum.java
+++ array/easy/TwoSum.java
@@ -1,15 +1,34 @@
 package easy;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
     public static void main(String[] args) {
-        System.out.println("Meow");
+        TwoSum tsum = new TwoSum();
+        int[] arr = {10, 4, 5, 6, 9, 2, 1};
+        int targ = 11;
+
+        // to show array in the right way just for checking:
+        System.out.println(Arrays.toString(tsum.twoSum(arr, targ)));
     }
 
 
-    //ublic int[] twoSum(int[] nums, int target) {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> table = new HashMap<>(); // for storing variables and indexes;
 
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i]; // to get difference to further search for desired number;
 
-
-    //return ?;
+            if (table.containsKey(diff)) {
+                /* if this value (diff) is already in the table (in fact, equal some element from "nums"),
+                so we got both of our variables and can return them indexes: */
+                return new int[]{table.get(diff), i};
+            }
+            table.put(nums[i], i); // adding each pair element(key)-index(value) in the table;
+        }
+        // exception if we can't find desired pair:
+        throw new IllegalArgumentException("No two sum solution");
     }
 }
