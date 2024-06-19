b15e8ee60e8d1e3ca4132db6daf01d894d23b130
VikkMoor <moorvikk@gmail.com>
2024-06-19 11:21:46 +0500

    3sumstarted

---
+++ array/medium/ThreeSum.java
@@ -0,0 +1,18 @@
+package medium;
+
+import java.util.List;
+
+public class ThreeSum {
+    public static void main(String[] args) {
+        ThreeSum tsum = new ThreeSum();
+        int[] arr = {-1,0,1,2,-1,-4};
+        // to show array in the right way just for checking:
+        System.out.println(tsum.threeSum(arr));
+    }
+
+    public List<List<Integer>> threeSum(int[] nums) {
+
+
+
+    }
+}
