e7b6bb4b60719540f75bda9ab97148c969c1406e
VikkMoor <moorvikk@gmail.com>
2024-06-21 21:15:23 +0500

    3sumsDoesn'tWorks;c

--- array/medium/ThreeSum.java
+++ array/medium/ThreeSum.java
@@ -1,18 +1,44 @@
 package medium;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class ThreeSum {
     public static void main(String[] args) {
         ThreeSum tsum = new ThreeSum();
-        int[] arr = {-1,0,1,2,-1,-4};
+        int[] arr = {-1, 0, 1, 2, -1, -4};
         // to show array in the right way just for checking:
         System.out.println(tsum.threeSum(arr));
     }
 
     public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums); // for make it possible to solve with "two pointers";
 
+        List<List<Integer>> list = new ArrayList<>(); // to store ready triplets;
 
-
+        for (int i = 0; i < nums.length; i++) {
+            int firstNum = nums[i];
+            int leftPointer = i + 1;
+            int rightPointer = nums.length - 1;
+            while (leftPointer < rightPointer) {
+                int secondNum = nums[leftPointer];
+                int thirdNum = nums[rightPointer];
+                int currentSum = firstNum + secondNum + thirdNum;
+                if (currentSum == 0) {
+                    list.add((Arrays.asList(new Integer[]{firstNum, secondNum, thirdNum})));
+                    leftPointer++;
+                    while (nums[leftPointer] == nums[leftPointer - 1] && leftPointer < rightPointer){
+                        leftPointer++;
+                    }
+                } else if (currentSum > 0) {
+                    rightPointer--;
+                } else if (currentSum < 0) {
+                    leftPointer++;
+                }
+            }
+        }
+        return list;
     }
 }
+
+
