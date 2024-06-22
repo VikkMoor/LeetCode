a942cb3803c855155e8b21a75b352295feb99cc8
VikkMoor <moorvikk@gmail.com>
2024-06-22 20:33:37 +0500

    simplify path started

---
+++ string/medium/SimplifyPath.java
@@ -0,0 +1,16 @@
+package medium;
+
+public class SimplifyPath {
+
+    public static void main(String[] args) {
+        SimplifyPath path = new SimplifyPath();
+        String test = "/home//foo/";
+
+        System.out.println(path.simplifyPath(test));
+    }
+
+    public String simplifyPath(String path) {
+
+
+    }
+}
